7ac90336e6180ed66564033aeab0a3eee3867bc5
Aditya-o72 <adityakburman@gmail.com>
2026-04-09 00:40:38 +0530

    big change

---
+++ src/aboutcontact.js
@@ -0,0 +1,7 @@
+function openbox(page){
+    document.getElementById("popup").style.display="block";
+    document.getElementById("frame").src=page;
+}
+function closeBox(){
+    document.getElementById("popup").style.display="none";
+}
--- src/main.js
+++ src/main.js
@@ -45,15 +45,9 @@ orb.enableDamping = true;
 orb.target.set(0, .9, 0);
 orb.update();
 
-// window.addEventListener('resize', ()=>{
-//     cam.aspect = cont.clientWidth/cont.clientHeight; // setting the aspect ratio of the camera when the window is resised
-//     cam.updateProjectionMatrix();
-//     ren.setSize(cont.clientWidth, cont.clientHeight); // setting the size of the render when the window is resised
-// });
-
 // creating the gltf loader ///////////////////////
 const lo = new GLTFLoader();
-let model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11;
+let model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11;
 lo.load('/integumentary.glb', (gltf) => {
     model = gltf.scene;
     sc.add(model);
@@ -63,17 +57,14 @@ lo.load('/integumentary.glb', (gltf) => {
     console.log("exploring the individual parts");
     model.traverse((child) => {
         console.log("- Name : ", child.name, ", Type : ", child.type);
-
     })
-
-
 
 
 }, undefined, (error) => {
     console.log("the error", error);
 });
 
-function rm (a,b,c,d,e,f,g,h,i,j,k){
+function rm(a, b, c, d, e, f, g, h, i, j, k) {
     sc.remove(a);
     sc.remove(b);
     sc.remove(c);
@@ -89,232 +80,231 @@ function rm (a,b,c,d,e,f,g,h,i,j,k){
 
 let but1 = document.querySelector("#b1");
 but1.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/muscular.glb', (gltf) => {
-    model2 = gltf.scene;
-    sc.add(model2);
+        model2 = gltf.scene;
+        sc.add(model2);
 
 
 
-    console.log("exploring the individual parts");
-    model2.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
-
-    })
-
+        console.log("exploring the individual parts");
+        model2.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
+        })
 
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but2 = document.querySelector("#b2");
 but2.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/skeletal.glb', (gltf) => {
-    model3 = gltf.scene;
-    sc.add(model3);
+        model3 = gltf.scene;
+        sc.add(model3);
 
 
 
-    console.log("exploring the individual parts");
-    model3.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model3.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but3 = document.querySelector("#b3");
 but3.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/reproductive.glb', (gltf) => {
-    model4 = gltf.scene;
-    sc.add(model4);
+        model4 = gltf.scene;
+        sc.add(model4);
 
 
 
-    console.log("exploring the individual parts");
-    model4.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model4.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but4 = document.querySelector("#b4");
 but4.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/nervous.glb', (gltf) => {
-    model5 = gltf.scene;
-    sc.add(model5);
+        model5 = gltf.scene;
+        sc.add(model5);
 
 
 
-    console.log("exploring the individual parts");
-    model5.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model5.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but5 = document.querySelector("#b5");
 but5.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/digestive.glb', (gltf) => {
-    model6 = gltf.scene;
-    sc.add(model6);
+        model6 = gltf.scene;
+        sc.add(model6);
 
 
 
-    console.log("exploring the individual parts");
-    model6.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model6.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but6 = document.querySelector("#b6");
 but6.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/circulatory.glb', (gltf) => {
-    model7 = gltf.scene;
-    sc.add(model7);
+        model7 = gltf.scene;
+        sc.add(model7);
 
 
 
-    console.log("exploring the individual parts");
-    model7.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model7.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but7 = document.querySelector("#b7");
 but7.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/renal.glb', (gltf) => {
-    model8 = gltf.scene;
-    sc.add(model8);
+        model8 = gltf.scene;
+        sc.add(model8);
 
 
 
-    console.log("exploring the individual parts");
-    model8.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model8.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but8 = document.querySelector("#b8");
 but8.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/respiratory.glb', (gltf) => {
-    model9 = gltf.scene;
-    sc.add(model9);
+        model9 = gltf.scene;
+        sc.add(model9);
 
 
 
-    console.log("exploring the individual parts");
-    model9.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model9.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but9 = document.querySelector("#b9");
 but9.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/endocrine.glb', (gltf) => {
-    model10 = gltf.scene;
-    sc.add(model10);
+        model10 = gltf.scene;
+        sc.add(model10);
 
 
 
-    console.log("exploring the individual parts");
-    model10.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model10.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 let but10 = document.querySelector("#b10");
 but10.addEventListener("click", () => {
-    rm(model, model2, model3, model4, model5, model6,model7, model8, model9, model10, model11);
+    rm(model, model2, model3, model4, model5, model6, model7, model8, model9, model10, model11);
     lo.load('/integumentary.glb', (gltf) => {
-    model11 = gltf.scene;
-    sc.add(model11);
+        model11 = gltf.scene;
+        sc.add(model11);
 
 
 
-    console.log("exploring the individual parts");
-    model11.traverse((child) => {
-        console.log("- Name : ", child.name, ", Type : ", child.type);
+        console.log("exploring the individual parts");
+        model11.traverse((child) => {
+            console.log("- Name : ", child.name, ", Type : ", child.type);
 
-    })
-
-
+        })
 
 
-}, undefined, (error) => {
-    console.log("the error", error);
-});
+
+
+    }, undefined, (error) => {
+        console.log("the error", error);
+    });
 });
 
 window.addEventListener('resize', () => {
